3a258f190d46f74961a16cf6c622f240182a3dc9
lzq <2476452614@qq.com>
2026-09-15 20:50:06 +0800

    Mapper1

--- src/main/java/com/lzq/shortlink/ShortLinkApplication.java
+++ src/main/java/com/lzq/shortlink/ShortLinkApplication.java
@@ -1,9 +1,11 @@
 package com.lzq.shortlink;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.lzq.shortlink.mapper")
 public class ShortLinkApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/lzq/shortlink/mapper/ShortLinkMapper.java
@@ -0,0 +1,8 @@
+package com.lzq.shortlink.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lzq.shortlink.entity.ShortLink;
+
+public interface ShortLinkMapper extends BaseMapper<ShortLink> {
+}
